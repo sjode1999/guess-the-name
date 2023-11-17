f14e106359f2da89c4cb5d967e49a4d80c43f126
Sajad Ali <sjodealosh@gmail.com>
2023-11-17 00:32:15 -0700

    Guessing Game

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-# This is a sample Python script.
-g
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import random
 
-def guessing_game():
-    print("Welcome to the Guessing Game!")
-    secret_number = random.randint(1, 10)
+
+
+def Guess_Game():
+    print("Welcome to the guessing game")
+    Real_Number = random.randint(1, 10)
     attempts = 0
 
     while True:
-        user_guess = int(input("Guess the number (between 1 and 10): "))
+        try:
+            Guess_Number = int(input("Guess the number between (1 and 10): "))
+        except ValueError:
+            print('Invalid input. Please enter a valid number.')
+            continue
+
         attempts += 1
 
-        if user_guess == secret_number:
-            print(f"Congratulations! You guessed the correct number in {attempts} attempts.")
+        if Guess_Number == Real_Number:
+            print(f"Congratulations, you guessed in {attempts} attempts.")
             break
+        elif Guess_Number < Real_Number:
+            print("Wrong Answer! Try Higher.")
+        else:
+            print("Wrong Answer! Try Lower.")
 
-            whi
+    while True:
+        Another_attempt = input('Would you like to try again (Y/N): ')
+        if Another_attempt.upper() == 'N':
+            break
+        else:
+            print('Invalid Selection')
+
+if __name__ == "__main__":
+    Guess_Game()
