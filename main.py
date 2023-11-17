248dc9474f4259c12afc3076a0ac991a3fdbacf0
Sajad Ali <sjodealosh@gmail.com>
2023-11-16 23:04:18 -0700

    guess the name

--- main.py
+++ main.py
@@ -1,80 +1,19 @@
 # This is a sample Python script.
-
+g
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import random
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-import sys
-
-
-def add(x, y):
-    return x + y
-
-def subtract(x, y):
-    return x - y
-
-
-def multiply(x, y):
-    return x * y
-
-
-def divide(x, y):
-    return x/y
-
-
-print("User Menu")
-print("1. to add ")
-print("2. to subtract")
-print("3. to multiply")
-print("4. to divide")
-print("5. to exit")
-
-while True:
-    choice = int(input("Enter Choice (1/2/3/4/5): "))
-
-    if choice == 5:
-        print("Thank you for using calculator!!!")
-        break
-
-    if choice not in (1, 2, 3, 4):
-        print("Invalid Selection, Please Try Again")
-        continue
-
-    num1 = int(input("Enter the first number: "))
-    while True:
-        num2 = int(input("Enter the second number: "))
-        if choice == 4 and num2 == 0:
-            print('Cannot divide by 0. Are you dumb?')
-            continue
-        else:
-            break
-
-    if choice == 1:
-        print(num1, "+", num2, "=", add(num1, num2))
-    elif choice == 2:
-        print(num1, "-", num2, "=", subtract(num1, num2))
-    elif choice == 3:
-        print(num1, "*", num2, "=", multiply(num1, num2))
-    elif choice == 4:
-        print(num1, "/", num2, "=", divide(num1, num2))
+def guessing_game():
+    print("Welcome to the Guessing Game!")
+    secret_number = random.randint(1, 10)
+    attempts = 0
 
     while True:
-        another_calculation = input("Would you like to make a new calculation (Y/N): ")
-        if another_calculation.upper() == "N":
-            sys.exit()
-        if another_calculation.upper() == "Y":
-            break
-        print("Invalid Selection")
-        continue
+        user_guess = int(input("Guess the number (between 1 and 10): "))
+        attempts += 1
 
-        print('hello')
+        if user_guess == secret_number:
+            print(f"Congratulations! You guessed the correct number in {attempts} attempts.")
+            break
